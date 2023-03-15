5d667d2da33050655fa3a2bc841d81535dbde6ed
AntonBuus <antonbuushansen@gmail.com>
2023-03-15 23:00:22 +0100

    ændrede lidt

--- Assets/Group Folders/Anton/UIManager.cs
+++ Assets/Group Folders/Anton/UIManager.cs
@@ -2,28 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject menu;
-   
-    public static bool GameIsPaused = false;
     
+    public Transform head;
+    public float spawnDistance = 2;
+    public InputActionProperty showMenuButton;
+
+    private Vector3 relativePosition;
+
     void Update()
     {
+        if (showMenuButton.action.WasPressedThisFrame())
+        {
+            Pause();
+            Debug.Log("Menu Pressed!");
 
+            if (menu.activeSelf)
+            {
+                menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
+                
+            }
+        }
+
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.forward *= -1;
     }
+    //----------------------------------------
 
+    public static bool GameIsPaused = false;
+    
+   
     void Resume()
     {
-        menu.SetActive(false);
+        menu.SetActive(menu.activeSelf);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     void Pause()
     {
-        menu.SetActive(true);
+        menu.SetActive(!menu.activeSelf);
+        
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
