752e0d0b303f3bf434fde77bea11bee6c26acd17
Daniel Lerche <daniel.lerche10@gmail.com>
2023-03-17 09:06:52 +0100

    DataCollection stuff

---
+++ Assets/Group Folders/Daniel/Scripts/DataCollection/CSVFile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+
+public struct CSVFile
+{
+    public string Path { get; private set; }
+
+    public CSVFile(string path)
+    {
+        Path = path;
+    }
+    
+    
+    
+}
