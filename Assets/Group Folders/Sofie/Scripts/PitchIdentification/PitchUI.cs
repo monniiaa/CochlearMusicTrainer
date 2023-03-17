ce9455f5dcbca035fa9e23f086be8c25e0e8be10
monniiaa <74655535+monniiaa@users.noreply.github.com>
2023-03-17 13:01:10 +0100

    Created more game logic

---
+++ Assets/Group Folders/Sofie/Scripts/PitchIdentification/PitchUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PitchUI : MonoBehaviour
+{
+    [SerializeField]
+    Button contineButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contineButton.onClick.AddListener(loadMenuScene);
+    }
+
+    public void loadMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
