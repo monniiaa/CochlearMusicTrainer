fa060cdb8d24f947243c01e3d914b4d70e5225ab
Daniel Lerche <daniel.lerche10@gmail.com>
2023-03-29 16:57:11 +0200

    Switched to JSON with working generic data container parsing to JSON

---
+++ Assets/Group Folders/Daniel/Scripts/DataCollection/JsonManager.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class JsonManager : MonoBehaviour
+{
+    private static JsonManager _instance;   
+    public static JsonManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<JsonManager>();
+            }
+
+            return _instance;
+        }
+    }
+    
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Start()
+    {
+        WriteDataToFile<SoundLocalizationDataContainer>(new SoundLocalizationDataContainer(10, 1));
+        WriteDataToFile<SoundLocalizationDataContainer>(new SoundLocalizationDataContainer(12, 1));
+        WriteDataToFile<SoundLocalizationDataContainer>(new SoundLocalizationDataContainer(10, 1));
+        WriteDataToFile<SoundLocalizationDataContainer>(new SoundLocalizationDataContainer(10, 90));
+    }
+
+    private static void WriteDataToFile<T>(AbstractDataContainer dataContainer) where T : AbstractDataContainer
+    {
+        string fullPath = Application.dataPath + "\\TestDataFiles\\" + dataContainer.Path;
+        Debug.Log(Application.dataPath);
+        Debug.Log(fullPath);
+        if(!Directory.Exists(fullPath)) Directory.CreateDirectory(Path.GetDirectoryName(fullPath) ?? throw new InvalidOperationException());
+
+        DataContainerList<T> dataContainerList;
+        if (File.Exists(fullPath))
+        {
+            dataContainerList = JsonUtility.FromJson<DataContainerList<T>>(File.ReadAllText(fullPath));
+        }
+        else
+        {
+            dataContainerList = new DataContainerList<T>();
+        }
+
+        dataContainerList.DataList.Add((T) dataContainer);
+
+        using (FileStream stream = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write))
+        {
+            stream.Write(new UTF8Encoding(true).GetBytes(dataContainerList.ToJson()));
+        }
+    }
+}
